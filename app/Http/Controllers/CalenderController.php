4830338701d932e7ad38324b71a2361004989593
ali123-aliu <64396082+ali123-aliu@users.noreply.github.com>
2023-05-26 22:47:13 +0500

    skip_+days

--- app/Http/Controllers/CalenderController.php
+++ app/Http/Controllers/CalenderController.php
@@ -14,24 +14,38 @@ class CalenderController extends Controller
     }
     public function store(Request $request)
     {
-        $start_datetime = Carbon::parse($request->start_date);
-        $end_datetime = Carbon::parse($request->end_date);
+        $skip_dates = ['2023-05-29'];
+        $working_hours = $this->calculate($request->start_date,$request->end_date,$skip_dates);
+        return response([
+            'Start Time' => Carbon::parse($request->start_date)->format('Y-m-d H:i:s'),
+            'End Time' => Carbon::parse($request->end_date)->format('Y-m-d H:i:s'),
+            'Skip Dates' => $skip_dates,
+            'Working Hours' => $working_hours
+            ]);
+    }
+
+
+    public function calculate($start, $end, $skip_dates = [])
+    {
+        $start_datetime = Carbon::parse($start);
+        $end_datetime = Carbon::parse($end);
         $working_hours = 0;
         $iteration = 0;
         $total_days = $start_datetime->diffInDays($end_datetime);
         while ($start_datetime <= $end_datetime) {
             if ($start_datetime->dayOfWeek !== Carbon::SUNDAY) {
-                $office_start = $start_datetime->copy()->setHour(8)->setMinute(0)->setSecond(0);
-                $office_end = $start_datetime->copy()->setHour(17)->setMinute(0)->setSecond(0);
-                if($iteration === 0){
-                    $office_start = ($start_datetime);
+                if(!in_array($start_datetime->format('Y-m-d'),$skip_dates)){
+                    $office_start = $start_datetime->copy()->setHour(8)->setMinute(0)->setSecond(0);
+                    $office_end = $start_datetime->copy()->setHour(17)->setMinute(0)->setSecond(0);
+                    if($iteration === 0){
+                        $office_start = ($start_datetime);
+                    }
+                    if ($iteration === $total_days) {
+                        $office_end = $end_datetime;
+                    }
+                    $working_hours += $office_end->diffInSeconds($office_start);
                 }
-                if ($iteration === $total_days) {
-                    $office_end = $end_datetime;
-                }
-                $working_hours += $office_end->diffInSeconds($office_start);
             }
-
             $start_datetime->addDay();
             $iteration++;
         }
@@ -41,14 +55,6 @@ class CalenderController extends Controller
         $hours = $hours<10 ? '0'.$hours : $hours;
         $minutes = $minutes<10 ? '0'.$minutes : $minutes;
 
-        $working_time = $hours.':'.$minutes.':00';
-
-        return response([
-            'Start Time' => Carbon::parse($request->start_date)->format('Y-m-d H:i:s'),
-            'End Time' => Carbon::parse($request->end_date)->format('Y-m-d H:i:s'),
-            'Working Time' => $working_time
-            ]);
-
-
+        return $hours.':'.$minutes.':00';
     }
 }
--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -56,67 +56,66 @@
         // }
     </script>
 
+{{--    <script>--}}
+{{--        $(document).ready(function() {--}}
+{{--            var start_date = '2023-05-01 10:35:24'--}}
+{{--            console.log(start_date)--}}
+{{--        })--}}
+
+{{--    </script>--}}
+
     <script>
+        var disable_dates = [];
         $(document).ready(function() {
-            var start_date = '2023-05-01 10:35:24'
-            console.log(start_date)
-        })
+            function handleDateSelection(date,cell) {
+                // cell.addClass('disabled');
+                var formattedDate = date.format('YYYY-MM-DD');
 
-    </script>
+                if ($.inArray(date.format('YYYY-MM-DD'), disable_dates) === -1 ) {
+                    cell.addClass('disabled');
+                }
 
-    <script>
-        // var disable_dates = [];
-        // $(document).ready(function() {
-        //     function handleDateSelection(date,cell) {
-        //         // cell.addClass('disabled');
-        //         console.log('sdf')
-        //         var formattedDate = date.format('YYYY-MM-DD');
-        //
-        //         if ($.inArray(date.format('YYYY-MM-DD'), disable_dates) === -1 ) {
-        //             cell.addClass('disabled');
-        //         }
-        //
-        //         // console.log(formattedDate);
-        //         // Perform any additional operations on the selected date
-        //     }
-        //     $('#calendar').fullCalendar({
-        //         header: false, // Hide the header with navigation buttons
-        //         defaultView: 'month', // Show only a single month at a time
-        //         selectable: true,
-        //         select: function(start, end, jsEvent, view) {
-        //             var selectedDate = start.format('YYYY-MM-DD');
-        //             var numberToPush = selectedDate;
-        //
-        //             if (disable_dates.indexOf(numberToPush) === -1) {
-        //                 disable_dates.push(numberToPush);
-        //             }
-        //             disable_date();
-        //         },
-        //
-        //         dayClick: function(date, jsEvent, view) {
-        //             var cell = $(jsEvent.target).closest('.fc-day');
-        //             handleDateSelection(date,cell);
-        //
-        //         },
-        //         dayRender: function(date, cell) {
-        //
-        //             if (date.day() === 0 ) {
-        //                 cell.addClass('disabled');
-        //             }
-        //         },
-        //         selectAllow: function(selectInfo) {
-        //             var selectedDate = selectInfo.start.format('YYYY-MM-DD');
-        //             var dayOfWeek = selectInfo.start.day();
-        //             if (dayOfWeek === 0) {
-        //                 return false; // Disable selection on Sundays
-        //             }
-        //             return true; // Allow selection on other days
-        //         }
-        //     });
-        // });
-        // function disable_date(){
-        //     console.log(disable_dates)
-        // }
+                // console.log(formattedDate);
+                // Perform any additional operations on the selected date
+            }
+            $('#calendar').fullCalendar({
+                header: false, // Hide the header with navigation buttons
+                defaultView: 'month', // Show only a single month at a time
+                selectable: true,
+                select: function(start, end, jsEvent, view) {
+                    var selectedDate = start.format('YYYY-MM-DD');
+                    var numberToPush = selectedDate;
+
+                    if (disable_dates.indexOf(numberToPush) === -1) {
+                        disable_dates.push(numberToPush);
+                    }
+                    disable_date();
+                },
+
+                dayClick: function(date, jsEvent, view) {
+                    var cell = $(jsEvent.target).closest('.fc-day');
+                    handleDateSelection(date,cell);
+
+                },
+                dayRender: function(date, cell) {
+
+                    if (date.day() === 0 ) {
+                        cell.addClass('disabled');
+                    }
+                },
+                selectAllow: function(selectInfo) {
+                    var selectedDate = selectInfo.start.format('YYYY-MM-DD');
+                    var dayOfWeek = selectInfo.start.day();
+                    if (dayOfWeek === 0) {
+                        return false; // Disable selection on Sundays
+                    }
+                    return true; // Allow selection on other days
+                }
+            });
+        });
+        function disable_date(){
+            console.log(disable_dates)
+        }
     </script>
     <style>
         .disabled {
